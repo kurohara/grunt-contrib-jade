c5d0972794f9136bbb99923e4e6463c615b60cf3
Hiroyoshi Kurohara <kurohara@yk.rim.or.jp>
2015-04-22 12:07:15 +0900

    Added the ability to append language add-on like 'jade-php'

--- tasks/jade.js
+++ tasks/jade.js
@@ -59,6 +59,15 @@ module.exports = function(grunt) {
 
         try {
           var jade = f.orig.jade = require('jade');
+          // 
+          // in Grungfile, you can set language add-on like 'jade-php' like as:
+          // options: { languageaddon: [ list of language add-on module names ] ...
+          //
+          if (typeof options['languageaddon'] !== undefined) {
+            options.languageaddon.forEach(function(f) {
+              require(f)(jade);
+            });
+          }
           if (typeof data === 'function') {
             // if data is function, bind to f.orig, passing f.dest and f.src
             f.orig.data = data.call(f.orig, f.dest, f.src);
